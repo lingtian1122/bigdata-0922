73d46b2a75665db914877598c98ec9757faac406
lingtian1122 <2874574228@qq.com>
2019-09-22 23:04:47 +0800

    Initial commit

---
+++ src/main/java/com/zhouxl/user/Test.java
@@ -0,0 +1,14 @@
+package com.zhouxl.user;
+
+/**
+ * bigdata-0922
+ * <p>
+ * com.zhouxl.user
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
